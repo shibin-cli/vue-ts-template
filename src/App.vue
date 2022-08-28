74cc0e2ebbe87ee1196216c8a216b147153ef83e
shibin <shibiny6@gmail.com>
2022-08-28 22:00:10 +0800

    feat: 🎸 init

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,9 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
 import { ref } from 'vue'
 const count = ref(0)
 </script>
 
 <template>
-  <HelloWorld
-    title="helloworld"
-    :count="count"
-  />
   {{ count }}
   <button @click="count++">
     +
