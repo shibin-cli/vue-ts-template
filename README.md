299421b97d6420106aa5aed6bf1148a296948718
shibin <shibiny6@gmail.com>
2022-08-28 21:58:39 +0800

    feat: 🎸 init

---
+++ README.md
@@ -0,0 +1,20 @@
+# Vue 3 + TypeScript + Vite + Eslint
+Vue 3 template
+```bash
+git clone https://github.com/shibin-cli/vue-ts-template.git
+cd vue-ts-template
+pnpm i
+npm run dev
+```
+
+Recommended editor configuration
+```json
+// .vscode/settings.json
+{
+    "editor.formatOnSave": true,
+    "editor.codeActionsOnSave": {
+        "source.fixAll.eslint": true
+    },
+    "editor.tabSize": 2
+}
+```
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { watchEffect } from 'vue'
+
+interface Props {
+  title: string
+  count: number
+}
+const props = defineProps<Props>()
+const stop = watchEffect(() => {
+  console.log(props.title)
+  console.log(props.count)
+})
+setTimeout(() => {
+  stop()
+}, 3000)
+</script>
+
+<template>
+  <h1 />
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
